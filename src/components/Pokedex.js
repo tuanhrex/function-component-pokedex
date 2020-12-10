cc00f7ba3fe73dd694f4e18084c4910144f2150c
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-12-10 16:59:13 -0500

    [pokedex] Add useEffect

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios'
 
 function Pokedex () {
     const [pokemonName, setPokemonName] = useState('pikachu');
     const [imageUrl, setImageUrl] = useState('');
+
+    useEffect(() => {
+        if (pokemonName === '') {
+            return
+        }
+
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .then((res) => {
+            setImageUrl(res.data.sprites.other['official-artwork'].front_default)
+        }).catch((err) => {
+            setImageUrl('')
+        })
+      });
 
     return(
         <div>
